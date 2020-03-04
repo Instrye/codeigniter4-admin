834fa66b4408ee9c43e8b487643aa3bd6fe35e4f
Instrye <instrye@gmail.com>
2020-03-04 17:10:10 +0800

    fix. public vendor can't update

---
+++ app/Filters/Auth.php
@@ -0,0 +1,10 @@
+<?php namespace App\Filters;
+
+use CodeIgniter\HTTP\RequestInterface;
+use CodeIgniter\HTTP\ResponseInterface;
+use CodeIgniter\Filters\FilterInterface;
+
+class Auth extends FilterInterface
+{
+    
+}
