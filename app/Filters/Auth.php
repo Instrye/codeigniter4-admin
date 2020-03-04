e9699bea7c29dcb9bd85ef1abc778445ba723d19
Instrye <instrye@gmail.com>
2020-03-04 17:21:04 +0800

    auth filter

--- app/Filters/Auth.php
+++ app/Filters/Auth.php
@@ -4,7 +4,14 @@ use CodeIgniter\HTTP\RequestInterface;
 use CodeIgniter\HTTP\ResponseInterface;
 use CodeIgniter\Filters\FilterInterface;
 
-class Auth extends FilterInterface
+class Auth implements FilterInterface
 {
-    
+    public function before(RequestInterface $request)
+    {
+
+    }
+
+    public function after(RequestInterface $request, ResponseInterface $response)
+    {
+    }
 }
